9d653ad8a126d20fe478d8c7a1f2a8b0f21d954f
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-02-28 15:27:19 +0900

    feat: Start Page
    - 초기 시작 페이지

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,16 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import MainPage from "./pages/main-page/MainPage";
+import StartPage from "./pages/start-page/StartPage";
 
 const defaultRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/", element: <MainPage /> }],
+    children: [
+      { path: "/", element: <StartPage /> },
+      { path: "/mainpage", element: <MainPage /> },
+    ],
   },
 ]);
 
