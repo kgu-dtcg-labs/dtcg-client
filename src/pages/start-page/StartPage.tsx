0092e063ab23ee473e7ec20c5c2867fb0f28a77b
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-03-04 23:29:32 +0900

    refactor: 시작 페이지 css 수정

--- src/pages/start-page/StartPage.tsx
+++ src/pages/start-page/StartPage.tsx
@@ -3,8 +3,8 @@ import logo from "../../../public/logo.svg";
 
 const StartPage = () => {
   return (
-    <div className="flex flex-col items-center gap-10">
-      <img src={logo} alt="kgu" className="mt-52 max-w-[300px]" />
+    <div className="flex my-auto flex-col items-center gap-10">
+      <img src={logo} alt="kgu" className="max-w-[300px]" />
 
       <div className="flex flex-col gap-2 text-center text-[32px]">
         <h1>경기대학교 교통계획연구실</h1>
@@ -12,7 +12,7 @@ const StartPage = () => {
       </div>
 
       <Link to="/mainpage">
-        <button className="w-64 h-16 rounded-full bg-orange-500 font-bold text-white text-xl shadow-xl">
+        <button className="hover:bg-orange-400 w-64 h-16 rounded-full bg-orange-500 font-bold text-white text-xl shadow-md">
           시작하기
         </button>
       </Link>
