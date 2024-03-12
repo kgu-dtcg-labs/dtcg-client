3b63c7bbc24e846c3d1d7394ab95a0023aafaecd
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-03-12 11:03:09 +0900

    feat: 버튼 컴포넌트 black 색상 추가

--- src/components/common/Button/Button.style.tsx
+++ src/components/common/Button/Button.style.tsx
@@ -1,4 +1,5 @@
 export const buttonColors = {
   default: 'border-gray-500 text-gray-500 hover:bg-gray-100',
   blue: 'border-blue-500 text-blue-500 hover:bg-blue-100',
+  black: 'border-black bg-black text-white hover:bg-gray-800',
 };
