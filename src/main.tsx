67d76903dd44ef9dc058153f6cb73f0958494003
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-03-20 17:38:57 +0900

    feat: Context 적용
    - selectedCase를 모아놓을 context

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/globals.css';
 import App from './App.tsx';
+import { SelectedCaseProvider } from '@components/contexts/SelectedCaseContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SelectedCaseProvider>
+      <App />
+    </SelectedCaseProvider>
   </React.StrictMode>,
 );
