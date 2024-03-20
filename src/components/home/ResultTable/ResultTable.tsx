b19a2701dac97f2f8be67ee11105b858569eee6c
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-03-21 01:03:58 +0900

    refactor: ResultTable 데이터 표현 방법 변경
    - 단일 배열 매핑에서 이중 배열 매핑

--- src/components/home/ResultTable/ResultTable.tsx
+++ src/components/home/ResultTable/ResultTable.tsx
@@ -2,7 +2,7 @@ import { TABLE_HEADER } from '@constants/header';
 import { ElementType } from '@type/element';
 
 interface ResultProps {
-  result: ElementType[] | null;
+  result: ElementType[][] | null;
 }
 
 const ResultTable = ({ result }: ResultProps) => {
@@ -18,24 +18,26 @@ const ResultTable = ({ result }: ResultProps) => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          {result?.length === 0 ? (
-            <td className="px-4 py-2 text-left" colSpan={99}>
-              현재 생성된 데이터가 없습니다.
-            </td>
-          ) : (
-            <>
-              {result?.map((item, index) => (
-                <td
-                  key={index}
-                  className="px-4 py-1 text-center border bg-white dark:bg-gray-400"
-                >
-                  {item.name}
-                </td>
-              ))}
-            </>
-          )}
-        </tr>
+        {result?.length === 0 ? (
+          <tr>
+            <td className="px-4 py-2 text-left">데이터 없음</td>
+          </tr>
+        ) : (
+          <>
+            {result?.map((arr, index) => (
+              <tr key={index}>
+                {arr.map((item, index) => (
+                  <td
+                    key={index}
+                    className="px-4 py-1 text-center border bg-white dark:bg-gray-400"
+                  >
+                    {item.name}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </>
+        )}
       </tbody>
     </table>
   );
