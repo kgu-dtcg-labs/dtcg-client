bc409d9e26ab09fd86eea88181f23be3520bcced
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-25 22:05:42 +0900

    refactor: add select style to Element component

--- src/components/common/Element/Element.tsx
+++ src/components/common/Element/Element.tsx
@@ -4,18 +4,21 @@ import type { ElementColorType } from './Element.type';
 
 interface ElementProps extends React.HTMLAttributes<HTMLButtonElement> {
   color?: ElementColorType;
+  selected?: boolean;
 }
 
 const Element = ({
   color = 'case',
   className,
   children,
+  selected = false,
   ...rest
 }: ElementProps) => {
   return (
     <button
       className={classNames(
         'h-10 text-sm font-semibold rounded-lg w-36 dark:text-gray-800/80',
+        { '!border-orange-400 text-orange-400': selected },
         elementColors[color],
         className,
       )}
