366387c541c4cc2a3daa43ff49d843608dee4168
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-25 22:04:40 +0900

    refactor: add icon props to button component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -6,20 +6,23 @@ import { buttonColors } from './Button.style';
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   color?: ButtonColorType;
+  icon?: React.ReactNode;
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, className, color = 'default', ...props }, ref) => {
+  ({ children, className, color = 'default', icon, ...props }, ref) => {
     return (
       <button
         ref={ref}
         className={classNames(
           'border rounded py-1.5 px-2 transition-colors',
+          { 'flex items-center justify-center gap-1': icon },
           buttonColors[color],
           className,
         )}
         {...props}
       >
+        {icon}
         {children}
       </button>
     );
