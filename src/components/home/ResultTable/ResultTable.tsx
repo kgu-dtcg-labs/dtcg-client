f1d76b01160da68bf43f1e80293c6f3bed7936a2
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-22 23:44:40 +0900

    fix: add displayName in ResultTable Component

--- src/components/home/ResultTable/ResultTable.tsx
+++ src/components/home/ResultTable/ResultTable.tsx
@@ -11,9 +11,9 @@ const ResultTable = ({ result }: ResultProps) => {
     <table className="text-center table-fixed select-none">
       <thead>
         <tr className="font-medium border-b divide-x bg-slate-100 dark:bg-gray-500">
-          {TABLE_HEADER.map((item, index) => (
-            <td key={index} className="py-4 min-w-32">
-              {item.name}
+          {TABLE_HEADER.map(({ name }, index) => (
+            <td key={`${name}-${index}`} className="py-4 min-w-32">
+              {name}
             </td>
           ))}
         </tr>
@@ -27,12 +27,12 @@ const ResultTable = ({ result }: ResultProps) => {
           // TODO: 페이지네이션 구현
           result.slice(0, 100).map((arr, index) => (
             <tr key={index}>
-              {arr.map((item, index) => (
+              {arr.map(({ name }, index) => (
                 <td
-                  key={index}
+                  key={`${name}-${index}`}
                   className="px-4 py-1 text-center bg-white border dark:bg-transparent"
                 >
-                  {item.name}
+                  {name}
                 </td>
               ))}
             </tr>
@@ -43,9 +43,13 @@ const ResultTable = ({ result }: ResultProps) => {
   );
 };
 
-ResultTable.Wrapper = ({ children }: PropsWithChildren) => (
+const ResultTableWrapper = ({ children }: PropsWithChildren) => (
   <div className="mt-6 overflow-auto border rounded bg-gray-50 dark:bg-zinc-600">
     {children}
   </div>
 );
+ResultTableWrapper.displayName = 'ResultTableWrapper';
+
+ResultTable.Wrapper = ResultTableWrapper;
+
 export default ResultTable;
