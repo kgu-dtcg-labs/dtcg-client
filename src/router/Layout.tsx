da20bd455fbc9cb8d4493450202410c81204ecfb
gwansikk <seorit98@gmail.com>
2024-05-02 02:39:30 +0900

    chore: 레이아웃 개선

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className="dark:bg-zinc-800 dark:text-white/80 min-h-svh">
+    <div className="dark:bg-zinc-800 dark:text-white/80 min-h-svh flex flex-col justify-between">
       <Navbar />
       <main className="px-8">
         <Outlet />
