f2b7325a7f62d2b60bc2781bd298f10ac3eab24f
gwansikk <seorit98@gmail.com>
2024-05-02 00:45:55 +0900

    refactor: Add new types for Modal and Step

---
+++ src/types/common.ts
@@ -0,0 +1,10 @@
+/**
+ * @description
+ * 메인화면 Modal 상태
+ */
+export type Modal = '사고' | '법률' | '랜덤' | '선택' | '알림' | 'none';
+/**
+ * @description
+ * 페이지네이션 화면 이동 방향
+ */
+export type Step = 'back' | 'forward';
