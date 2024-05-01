dda9e218d25a806fa6b77180ba88d302f848cb02
gwansikk <seorit98@gmail.com>
2024-05-01 20:33:59 +0900

    chore: 서비스 이름 변경

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { WiDaySunny } from 'react-icons/wi';
-import { WiNightClear } from 'react-icons/wi';
+import { WiDaySunny, WiNightClear } from 'react-icons/wi';
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(
@@ -23,19 +22,17 @@ const Navbar = () => {
 
   return (
     <nav className="py-3 border-t-4 border-b border-t-blue-800 dark:border-zinc-600 dark:border-t-blue-900">
-      <div className="container flex items-center justify-between gap-4">
+      <div className="px-8 flex items-center justify-between gap-4">
         <div className="flex items-center gap-4">
           <img src="/logo.svg" alt="logo" className="max-w-[80px]" />
           <div className="text-nowrap">
             <h1 className="text-xs font-semibold">경기대학교 교통계획연구실</h1>
-            <h1 className="text-xl font-bold">시나리오 생성기</h1>
+            <h1 className="text-xl font-bold">테스트 케이스 생성기</h1>
           </div>
         </div>
-        <div>
-          <button onClick={toggleDarkMode}>
-            {darkMode ? <WiDaySunny size={32} /> : <WiNightClear size={32} />}
-          </button>
-        </div>
+        <button onClick={toggleDarkMode}>
+          {darkMode ? <WiDaySunny size={32} /> : <WiNightClear size={32} />}
+        </button>
       </div>
     </nav>
   );
