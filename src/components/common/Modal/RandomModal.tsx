b821386e5a96bafcb2c66713dbbf511a29c6cb37
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-06-18 17:44:45 +0900

    refactor: RandomModals

--- src/components/common/Modal/RandomModal.tsx
+++ src/components/common/Modal/RandomModal.tsx
@@ -3,13 +3,15 @@ import { Modal } from './Modal';
 import { useSetSelectedModalStore } from '@store/modal-type';
 import { Button } from '../Button/Button';
 import { useSetResultStore } from '@store/result';
-import { createScenarios } from '@utils/element';
+import { createTestCases } from '@utils/element';
 import { ElementType } from '@type/element';
-import { RandomType } from '@type/common';
 import { useGetCaseStore } from '@store/case';
+import Input from '../Input/Input';
 
 const RandomModal = () => {
-  const [count, setCount] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
+  const [description, setDescription] = useState<string>('');
+  const empty = count === 0 || description === '';
   const setModal = useSetSelectedModalStore();
   const setResult = useSetResultStore();
   const defaultCase = useGetCaseStore();
@@ -21,28 +23,50 @@ const RandomModal = () => {
     [],
   );
 
+  const handleDescriptionChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setDescription(e.target.value);
+    },
+    [],
+  );
+
   /**
    * @description 전체 케이스에서 시나리오를 랜덤으로 생성합니다.
    */
-  const handleRandomButtonClick = (cases: ElementType[], type: RandomType) => {
+  const handleRandomButtonClick = async (cases: ElementType[]) => {
     setModal('none');
-    setResult(createScenarios(cases, count, type));
+    const testCases = await createTestCases(cases, count, '랜덤', description);
+    setResult(testCases);
   };
 
   return (
     <Modal onClose={() => setModal('none')}>
-      <div className="grid items-center gap-2 text-center">
-        <h1 className="pb-4 text-xl font-semibold ">랜덤 생성 개수 입력</h1>
-        <span>생성할 시나리오의 개수를 입력해주세요.</span>
-        <span>최대 50,000개까지 입력 가능</span>
-        <input
-          value={count}
-          onChange={handleCountChange}
-          className="p-2 border rounded outline-none dark:bg-zinc-800"
-        />
+      <div className="grid items-center gap-8">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="pb-4 text-xl font-semibold ">랜덤 생성 개수 입력</h1>
+          <span>생성할 시나리오의 개수와 설명을 입력해주세요.</span>
+          <span>최대 50,000개까지 입력 가능</span>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <Input
+            label="테스트 케이스 개수"
+            value={count}
+            onChange={handleCountChange}
+            placeholder="생성할 테스트 케이스의 개수를 입력하세요"
+          />
+          <Input
+            label="설명"
+            value={description}
+            onChange={handleDescriptionChange}
+            placeholder="테스트 케이스에 대한 설명을 작성해주세요"
+          />
+        </div>
+
         <Button
-          color="black"
-          onClick={() => handleRandomButtonClick(defaultCase, '랜덤')}
+          color={`${empty ? 'disabled' : 'black'}`}
+          onClick={() => handleRandomButtonClick(defaultCase)}
+          disabled={empty}
         >
           생성하기
         </Button>
