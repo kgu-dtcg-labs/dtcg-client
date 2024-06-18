f79cb112a4a6520fafa3c599a4e368e9de7a828b
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-06-18 17:48:34 +0900

    refactor : update result.ts

--- src/store/result.ts
+++ src/store/result.ts
@@ -5,31 +5,31 @@ import {
   useSetRecoilState,
 } from 'recoil';
 
-import type { ElementType } from '@type/element';
+import type { TestCase } from '@type/element';
 
 /**
  * 결과 상태를 관리하는 atom입니다.
- * @type {import('recoil').RecoilState<ElementType[][]>}
+ * @type {import('recoil').RecoilState<TestCase>}
  */
-const resultState = atom<ElementType[][]>({
+const resultState = atom<TestCase>({
   key: 'result',
-  default: [],
+  default: { description: '', cases: [] },
 });
 
 /**
  * 시나리오 생성 결과 상태를 가져오고 설정합니다.
- * @returns {[ElementType[][], (newState: ElementType[][]) => void]} 현재 상태와 상태를 업데이트하는 함수.
+ * @returns {[TestCase, (newState: TestCase) => void]} 현재 상태와 상태를 업데이트하는 함수.
  */
 export const useResultStore = () => useRecoilState(resultState);
 
 /**
  * 시나리오 생성 결과 상태의 현재 값을 가져옵니다.
- * @returns {ElementType[][]} 결과 상태의 현재 값.
+ * @returns {TestCase} 결과 상태의 현재 값.
  */
 export const useGetResultStore = () => useRecoilValue(resultState);
 
 /**
  * 시나리오 생성 결과 상태를 설정합니다.
- * @returns {(newState: ElementType[][]) => void} 결과 상태를 업데이트하는 함수.
+ * @returns {(newState: TestCase) => void} 결과 상태를 업데이트하는 함수.
  */
 export const useSetResultStore = () => useSetRecoilState(resultState);
