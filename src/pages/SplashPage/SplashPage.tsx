fff76a16cdc703ad2c5e54dbbcb2eceb186e0d88
gwansikk <gwansik.kim@gwansik.dev>
2024-10-25 19:32:05 +0900

    feat: 프로그램 이름 변경

--- src/pages/SplashPage/SplashPage.tsx
+++ src/pages/SplashPage/SplashPage.tsx
@@ -7,7 +7,7 @@ const SplashPage = () => {
       <img src="/logo.svg" alt="logo" className="max-w-[300px]" />
       <div className="flex flex-col gap-2 text-3xl text-center">
         <h1>경기대학교 교통계획연구실</h1>
-        <h1>시나리오 생성기</h1>
+        <h1>주행 테스트 케이스 생성기</h1>
       </div>
       <Link to={PATH.HOME}>
         <button className="w-64 py-4 text-xl font-bold text-white bg-orange-500 rounded-full shadow-lg hover:bg-orange-400 dark:bg-orange-500/80 dark:hover:bg-orange-400/80">
