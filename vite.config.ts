330f119167b36b1e3bc017485becfea0431d6f3d
gwansikk <gwansik.kim@gwansik.dev>
2025-02-03 01:01:59 +0900

    chore: react swc 플러그인으로 교체

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/kgu-dtcg-labs/',
+  base: '/dtcg-client/',
   plugins: [react()],
   resolve: {
     alias: {
